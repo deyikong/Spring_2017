b427316c9ddd99d3a6a01875633a6e89ea3bcb24
HelloWare Academy <deyikong@hellowareacademy.com>
2017-01-08 15:02:04 -0800

    1-8-2017 notes

---
+++ 1-8-2017/Playground/Playground/Person_1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Playground
+{
+    class Person_1
+    {
+
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        
+
+        public void SetAddress(string city)
+        {
+            SetAddress(city, "FL");
+        }
+        //Don't Repeat Yourself: DRY
+        public void SetAddress(string city, string state)
+        {
+            Console.WriteLine("The City is " + city + ", The State is " + state);
+        }
+    }
+}
--- 1-8-2017/Playground/Playground/Properties/AssemblyInfo.cs
+++ 1-8-2017/Playground/Playground/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b4af2f24-e7ac-4840-8c28-a2019848a5f5")]
+[assembly: Guid("ec7156d8-da81-4858-b4ef-746246972b54")]
 
 // Version information for an assembly consists of the following four values:
 //

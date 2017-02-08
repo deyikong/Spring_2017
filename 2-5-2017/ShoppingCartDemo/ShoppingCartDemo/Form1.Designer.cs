eb679553155cfe974bbbe10328e4abcbda04ae47
HelloWare <deyikong@hellowareacademy.com>
2017-02-07 21:38:18 -0600

    shopping card demo

--- 2-5-2017/ShoppingCartDemo/ShoppingCartDemo/Form1.Designer.cs
+++ 2-5-2017/ShoppingCartDemo/ShoppingCartDemo/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,6 +51,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -69,11 +71,22 @@
             this.dataGridView1.Size = new System.Drawing.Size(749, 351);
             this.dataGridView1.TabIndex = 3;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(417, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(843, 441);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -92,6 +105,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- 2-5-2017/ShoppingCartDemo/ShoppingCartDemo/Form1.cs
+++ 2-5-2017/ShoppingCartDemo/ShoppingCartDemo/Form1.cs
@@ -22,28 +22,49 @@ namespace ShoppingCartDemo
         {
             try
             {
-                String firstName = textBox1.Text;
-                //This is not a good practice
-                String sql = "SELECT Id, FirstName, LastName, Email FROM dbo.Customer WHERE FirstName LIKE '%" +
-                    firstName + "%'";
-                String connectionString = @"Data Source=LAPTOP-E4GLUAC2\HELLOWARE; Integrated Security=SSPI;Initial Catalog=ShoppingCart";
-                SqlConnection sqlConnection = new SqlConnection(connectionString);
-
-                sqlConnection.Open();
-
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, sqlConnection);
-
-                DataSet customers = new DataSet();
-
-                sqlDataAdapter.Fill(customers, "Customers");
-                dataGridView1.AutoGenerateColumns = true;
-                dataGridView1.DataSource = customers.Tables[0];
+                GetData();
             }
             catch (Exception ex)
             {
 
             }
 
+        }
+
+        private void GetData()
+        {
+            String firstName = textBox1.Text;
+            //This is not a good practice
+            //Inline SQL, at risk of SQL Injection, Stored Procedure
+            String sql = "SELECT Id, FirstName, LastName, Email FROM dbo.Customer WHERE FirstName LIKE '%" +
+                firstName + "%'";
+            String connectionString = @"Data Source=LAPTOP-E4GLUAC2\HELLOWARE; Integrated Security=SSPI;Initial Catalog=ShoppingCart";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, sqlConnection);
+
+            DataSet customers = new DataSet();
+
+            sqlDataAdapter.Fill(customers, "Customers");
+            dataGridView1.AutoGenerateColumns = true;
+            //Check if there is any records
+            DataTable dataTable = customers.Tables[0];
+            if (dataTable.Rows.Count == 0)
+            {
+                label2.Text = "No records";
+            }
+            else
+            {
+                label2.Text = String.Empty;//""
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetData();
         }
     }
 }

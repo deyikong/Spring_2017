2fce38bf54830ac9172d484727d820423fa3bae4
HelloWare <deyikong@hellowareacademy.com>
2017-03-07 22:06:54 -0600

    notes

---
+++ SemesterProjects/ECom/HWA.ECom.Entity/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWA.ECom.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ SemesterProjects/ECom/HWA.ECom.Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using HWA.ECom.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWA.ECom.Repository
+{
+    class CategoryRepository
+    {
+
+        //CRUD, 
+        public Boolean Insert(Category category)
+        {
+            ////ADO.NET
+            //using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            //{
+            //    con.Open();
+            //    string query = "Insert into DemoStudent (FirstName, LastName, University, Gender) values('" + model.FirstName + "','" + model.LastName + "','" + model.University + "','" + model.Gender + "')";
+
+            //    SqlCommand cmd = new SqlCommand(query, con);
+            //    if (cmd.ExecuteNonQuery() == 1)
+            //        return true;
+            //}
+
+            return false;
+        }
+
+
+
+    }
+}
